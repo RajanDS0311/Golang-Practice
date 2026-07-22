af2132b626a0d38e74776abd97c00686693c5093
RajanDS0311 <rajanmadankar@gmail.com>
2026-07-23 00:37:30 +0530

    .......

--- 17_interface/Interface.go
+++ 17_interface/Interface.go
@@ -3,8 +3,13 @@ package main
 
 import "fmt"
 
+type paymenter interface {
+	pay(amount float32)
+	refund(amount float32, account string)
+}
+
 type payment struct {
-	gateway razorpay
+	gateway paymenter
 }
 
 func (p payment) makePayment(amount float32) {
@@ -13,7 +18,7 @@ func (p payment) makePayment(amount float32) {
 
 	// stripeGw.pay(100)
 
-	p.gateway.pay(100)
+	p.gateway.pay(amount)
 }
 
 type razorpay struct{}
@@ -22,17 +27,36 @@ func (r razorpay) pay(amount float32) {
 	fmt.Println("making payments using razorpay", amount)
 }
 
-type stripe struct{}
+// type stripe struct{}
 
-func (s stripe) pay(amount float32) {
-	fmt.Println("making payments using stripe", amount)
+// func (s stripe) pay(amount float32) {
+// 	fmt.Println("making payments using stripe", amount)
+// }
+
+type fakepayment struct{}
+
+func (f fakepayment) pay(amount float32) {
+	fmt.Println("Making payment using fake gateway for testing purpose")
 }
+
+type paypal struct{}
+
+func (p paypal) pay(amount float32) {
+	fmt.Println("Making payment using paypal", amount)
+}
+
+func (p paypal) refund(amount float32, account string) {
+	fmt.Println("")
+}
+
 func main() {
 
 	// stripeGw := stripe{}
-	razorpayGw := razorpay{}
+	// razorpayGw := razorpay{}
+	// fakeGw := fakepayment{}
+	paypalGw := paypal{}
 	newpayment := payment{
-		gateway: razorpayGw,
+		gateway: paypalGw,
 	}
 	newpayment.makePayment(100)
 }
